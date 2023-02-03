a5bfd828b9c179e90397cfe1456d39a6f3ead684
Driss Benomar <benomard999@gmail.com>
2023-02-03 18:16:57 +0100

    [DEV-119] add the siggnature method of matrix

--- MainServer/IUtilServer.java
+++ MainServer/IUtilServer.java
@@ -4,10 +4,20 @@ import java.util.List;
 import java.util.Stack;
 import java.awt.image.BufferedImage;
 
-public interface IUtilServer{
-    public Stack<BufferedImage> Decouper(File image ,int n) throws IOException ;
-    public byte[] Merge(List<Data> paries) throws IOException ;
-    public void DistToSlavers(Stack<BufferedImage> st , Stack<Worker> slavers, List<Data> filtredPartey,float[] kernel);
-    public void getAvailabelSlavers(File file, Stack<Worker>  slevers) throws IOException ;
-    public Data getItemById(int id,List<Data> list);
+public interface IUtilServer {
+    public Stack<BufferedImage> Decouper(File image, int n) throws IOException;
+
+    public byte[] Merge(List<Data> paries) throws IOException;
+
+    public void DistToSlavers(Stack<BufferedImage> st, Stack<Worker> slavers, List<Data> filtredPartey, float[] kernel);
+
+    public void getAvailabelSlavers(File file, Stack<Worker> slevers) throws IOException;
+
+    public Data getItemById(int id, List<Data> list);
+
+    public float[][] additionMatrice(float[][] matA, float[][] matB);
+
+    public float[][] substractionMatrice(float[][] matA, float[][] matB);
+
+    public float[][] multiplicationMatrice(float[][] matA, float[][] matB);
 }
