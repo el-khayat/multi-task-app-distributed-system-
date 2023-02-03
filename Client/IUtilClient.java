6377c17fce801fa73d1cbec5a710d64af2d5dd5d
mohamed elkhayat <elkhayat705@gmail.com>
2023-02-03 10:52:09 -0500

    add matrice op in client

--- Client/IUtilClient.java
+++ Client/IUtilClient.java
@@ -5,7 +5,7 @@ public interface IUtilClient {
     public String[] setInfoFromFile(String filepath) throws IOException;
     public float[] setKernelFromFile(String filepath) throws IOException;
     public File setImage(String filepath);
-    public float[][] ReadMatrice(); // read matrices, dimetion, operation, 
+    public float[][] ReadMatrice(); // read matrices, dimetion 
     public float[][] sendReciveMatrice(Socket socket,float[][] matrice);
     public void printMatrice(float[][] matice);
 }
